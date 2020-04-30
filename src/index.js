ed56d5d6461c6a44f4c0f8dde9d08c090812dece
mingy98 <ming_ying@college.harvard.edu>
2020-04-30 12:58:40 -0400

    merge fixed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-/**/import React from 'react';
+import React from 'react';
 import createSagaMiddleware from 'redux-saga';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
@@ -63,23 +62,3 @@ ReactDOM.render(
       </Router>,
   document.getElementById('root')
 )
-
-=======
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
->>>>>>> 641d8fc61c732ada8f5437e088c14eb035dc9128
