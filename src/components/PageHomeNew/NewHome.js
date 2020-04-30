c5490cf1e3c83ab41e18d0d8223c394a3a5c94e7
mingy98 <ming_ying@college.harvard.edu>
2020-04-30 13:07:15 -0400

    solution

--- src/components/PageHomeNew/NewHome.js
+++ src/components/PageHomeNew/NewHome.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container'
+import { storeGroceryItem } from '../../actions/index.js'
 import '../../static/App.css';
 
 
 class PageHome extends Component {
   constructor(props) {
     super(props)
-    this.state = { width: 0, height: 0, modalShow: false, showPopup: false}
+    this.state = { width: 0, height: 0, modalShow: false, showPopup: false, currentGroceryItem: ''}
   }
 
   componentDidMount() {
@@ -21,13 +23,39 @@ class PageHome extends Component {
     window.removeEventListener('resize', this.updateWindowDimensions)
   }
 
+  addItemToList = () => {
+    this.props.dispatch(storeGroceryItem(this.state.currentGroceryItem))
+  }
+
+  changeCurrentGroceryItem = (evt) => {
+    this.setState({currentGroceryItem: evt.target.value})
+  }
+
   render() {
     let modalClose = () => this.setState({ modalShow: false })
     if (this.state.width > 700 && this.state.modalShow) {
       modalClose()
     }
     return (
-      <div>Shopper</div>
+      <Container>
+        <div>
+          <div>
+            Enter a grocery Item Here:
+          </div>
+          <Button onClick = {this.addItemToList}>
+            Submit
+          </Button>
+          <input type = "text" onChange = {this.changeCurrentGroceryItem} placeholder = "Grocery Item Here"/>
+        </div>
+        <div>
+          <div>
+            Grocery List 
+          </div>
+          {this.props.groceryItems}
+          <div>
+          </div>
+        </div>
+      </Container>
     )
   }
 }
