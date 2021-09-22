aab1eb4a89267b357fdbeaa6d3a12b76640f20b2
Philippe Noël <phil@fractal.co>
2021-09-22 10:11:02 -0400

    remove shopper, outdated

--- questions/shopper/src/actions/index.js
+++
@@ -1,9 +0,0 @@
-export const STORE_GROCERY_ITEM = "STORE_GROCERY_ITEM"
-
-
-export function storeGroceryItem(item) {
-	return {
-		type: STORE_GROCERY_ITEM,
-		item
-	}
-}
--- solutions/shopper/README.md
+++
@@ -1,3 +0,0 @@
-# Solutions
-
-This folder contains the implementation solution to the Shopper question, which can be run with `npm install` followed by `npm start`.
